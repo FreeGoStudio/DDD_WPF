a90e66eedf2e85364ce3c7def236ae3916a0505c
蒋振华 <765643729@163.com>
2025-10-05 11:26:20 +0800

    删除多余的代码

--- InfrastructureLayer/AppDbContext.cs
+++ InfrastructureLayer/AppDbContext.cs
@@ -11,10 +11,5 @@ namespace InfrastructureLayer
         {
             options.UseSqlite("Data Source=app.db");
         }
-
-        public void ApplyMigrations()
-        {
-            Database.Migrate(); // 自动应用所有未执行的迁移
-        }
     }
 }
